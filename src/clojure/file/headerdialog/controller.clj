ec5043e3ca603f21770dfa29cd1d43b3e16729ec
Nathaniel Hartman <nathan@172.16.1.33>
2009-04-07 19:24:45 -0500

    added save-changes button to header dialog

--- src/clojure/file/headerdialog/controller.clj
+++ src/clojure/file/headerdialog/controller.clj
@@ -1,22 +1,44 @@
 (ns file.headerdialog.controller
   (:use util gutil global)
   (:import (javax.swing JTable JScrollPane JButton JPanel JDialog JTabbedPane)
+	   (org.jdesktop.swingx JXTable)
 	   (javax.swing.table DefaultTableModel)
 	   (java.awt Dimension)
 	   (net.miginfocom.swing MigLayout)))
 
+(defn init-save-button [header table]
+  (let [button (JButton. "Save Changes")]
+    (on-click button
+      (fn [e]
+	(dosync 
+	 (alter header assoc :descriptors
+		(for [row (range 0 (count (:descriptors @header)))]
+		  (let [mnemonic (.getValueAt table row 0)
+			unit (.getValueAt table row 1)
+			data (.getValueAt table row 2)
+			description (.getValueAt table row 3)]
+		    {:mnemonic mnemonic
+		     :unit unit
+		     :data data
+		     :description description}))))))
+    button))
+
 (defn init-header-tab [header]
   (swing-io!
    (let [model (DefaultTableModel.)
-	 table (JTable. model)
+	 table (JXTable. model)
 	 pane (JScrollPane. table)
+	 panel (JPanel. (MigLayout.))
+	 save-button (init-save-button header table)
 	 descriptors (:descriptors @header)]
      (doto model
        (.addColumn "mnemonic" (into-array Object (map :mnemonic descriptors)))
        (.addColumn "unit" (into-array Object (map :unit descriptors)))
        (.addColumn "data" (into-array Object (map :data descriptors)))
        (.addColumn "description" (into-array Object (map :description descriptors))))
-     pane)))
+     (doto panel
+       (.add pane "push, grow, wrap")
+       (.add save-button "align 50%")))))
 
 (defn- find-header [lasfile type]
   (find-first #(= type (:type (deref %))) (:headers @lasfile)))
@@ -47,8 +69,7 @@
     (doto dialog
       (.add panel)
       (.setSize (Dimension. 500 600))
-      (.setVisible true))
-    ))
+      (.setVisible true))))
 
 (defn init-edit-button [lasfile]
   (let [button (JButton. "Edit Headers")]
